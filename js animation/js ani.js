221b58e9b369fd4c9a44c19e6a4d897b9d42e211
clarence07 <93646538+clarence07@users.noreply.github.com>
2021-11-24 06:54:53 +0800

    Add files via upload

---
+++ js animation/js ani.js
@@ -0,0 +1,8 @@
+// typing animation
+
+var typed = new Typed(".typing", {
+    Strings:["Student","Photographer","Artist","Chat Associate"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
